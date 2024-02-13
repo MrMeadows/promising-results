1e965433b91fbd2619f3f58c9f1dceaa0880cc20
Sebastian <snhmeadows@outlook.com>
2024-02-12 21:33:55 -0500

    finishing through step 3

--- app.js
+++ app.js
@@ -7,6 +7,27 @@ myPromise.then((result) => {
     return slowMath.multiply(result, 2);
 }).then((nextResult) => {
     console.log(nextResult);
+    return slowMath.divide(nextResult, 4);
+}).then((nextResult) => {
+    console.log(nextResult);
+    return slowMath.subtract(nextResult, 3);
+}).then((nextResult) => {
+    console.log(nextResult);
+    return slowMath.add(nextResult, 98);
+}).then((nextResult) => {
+    console.log(nextResult);
+    return slowMath.remainder(nextResult, 2);
+}).then((nextResult) => {
+    console.log(nextResult);
+    return slowMath.multiply(nextResult, 50)
+}).then((nextResult) => {
+    console.log(nextResult);
+    return slowMath.remainder(nextResult, 40);
+}).then((nextResult) => {
+    console.log(nextResult);
+    return slowMath.add(nextResult, 32);
+}).then((nextResult) => {
+    console.log(`The final result is ${nextResult}.`)
 }).catch((error) => {
     console.error(error);
 });
